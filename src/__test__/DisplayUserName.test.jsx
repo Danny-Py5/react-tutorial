d7548de3b949663661324134594f9846e1f039fa
Daniel Olatunde <olatundedaniel943@gmail.com>
2025-05-23 23:16:13 +0100

    queryByText()

--- src/__test__/DisplayUserName.test.jsx
+++ src/__test__/DisplayUserName.test.jsx
@@ -5,7 +5,15 @@ import DisplayUserName from "./component/DisplayUserName.jsx";
 describe("DisplayUserName", () => {
   it("renders userName", () => {
     render(<DisplayUserName userName={"Daniel"} />);
-    const allNames = screen.getAllByText("Daniel");
-    allNames.forEach((name) => expect(name).toBeInTheDocument()); // or /Daniel/ if we are concern about the regEx;
+    expect(screen.queryByText("Daniel Olatunde")).toBeNull();
+
+    // queryByText and getByText works differently
+    // getByText : throws an error if the text not found while the queryByText doesn't
+
+    // the usefulness of queryByText is that we can use it to check for a text that is
+    // not in the document and since it returns null, use that opportunity to expect
+    // something like .not.toBeInTheDocument, .toBeNull. which allows us to expect
+    // on what not in the document instead of the error that would be thrown when
+    // we use getByText
   });
 });
