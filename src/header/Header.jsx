e327614278e959104588d3dbbae52e8a286d6fca
Daniel Olatunde <olatundedaniel943@gmail.com>
2025-05-23 14:46:18 +0100

    Update the dependency array

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -24,7 +24,7 @@ export default function Header() {
         link.removeEventListener("click", addActive);
       });
     };
-  });
+  }, []);
   return (
     <header>
       <div className="logo">
